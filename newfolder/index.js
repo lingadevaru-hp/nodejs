8d7b1405c799e5062b5512d1020a00377f883172
lingadevaru <contact@lingadevaru.in>
2025-03-13 15:14:10 +0530

    Describe your changes here

--- newfolder/index.js
+++ newfolder/index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+const todoData = [
+  { id: "1",title: "ABC" , desc:"", datetime:"12/02/2025 10:15 AM"},
+  { id: "2",title: "ABC" , desc:"", datetime:"12/02/2025 10:15 AM"},
+  { id: "3",title: "ABC" , desc:"", datetime:"12/02/2025 10:15 AM"},
+  { id: "4",title: "ABC" , desc:"", datetime:"12/02/2025 10:15 AM"},
+  { id: "5",title: "ABC" , desc:"", datetime:"12/02/2025 10:15 AM"}
+  
+];
+
+app.get('/getTodoData',function (req, res) {
+    res.send(todoData)
+})
+
+
+
+app.listen(3000, function() {
+    console.log("Server strted listening on port 3000")
+})
